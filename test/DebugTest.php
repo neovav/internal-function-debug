ab36a019448b3b924fd5bf215e5857cb674204c6
Neovav <neovav@outlook.com>
2020-11-20 15:58:49 +0200

    Add methods for unit test

--- test/DebugTest.php
+++ test/DebugTest.php
@@ -1,8 +1,29 @@
 <?php
+namespace Test;
+
 use InternalFunctionsDebug\Debug;
 
-class DebugTest extends PHPUnit\Framework\TestCase
+function abs(...$args) {return Debug::exec(__FUNCTION__, $args);}
+
+class DebugTest extends \PHPUnit\Framework\TestCase
 {
+    protected $fixture;
+
+    public static array $args = [];
+
+    public function setUp(): void
+    {
+        $class = new \ReflectionClass('InternalFunctionsDebug\Debug');
+        $this->fixture = $class->getProperty('handler');
+        $this->fixture->setAccessible(true);
+    }
+
+    public function tearDown(): void
+    {
+        $this->fixture->setValue([]);
+        DebugTest::$args = [];
+    }
+
     /**
      * @var string $name
      * @var callable $handler
@@ -15,11 +36,8 @@ class DebugTest extends PHPUnit\Framework\TestCase
      */
     public function testSetHandler(string $name, callable $handler, bool $isBefor, array $result)
     {
-        $class = new ReflectionClass('InternalFunctionsDebug\Debug');
-        $property = $class->getProperty('handler');
-        $property->setAccessible(true);
         Debug::setHandler($name, $handler, $isBefor);
-        $this->assertEquals($result, $property->getValue());
+        $this->assertEquals($result, $this->fixture->getValue());
     }
 
     public function providerSetHandler() {
@@ -27,7 +45,65 @@ class DebugTest extends PHPUnit\Framework\TestCase
         $handler1 = function($name, $arguments, $result) {};
         return [
             ['abs', $handler0, true, ['befor' => ['abs' => [$handler0]]]],
-            ['abs', $handler0, false, ['befor' => ['abs' => [$handler0]], 'after' => ['abs' => [$handler1]]]]
+            ['abs', $handler0, false, ['after' => ['abs' => [$handler1]]]]
+        ];
+    }
+
+    /**
+     * @var string $name
+     * @var callable $handler
+     * @var int $number
+     * @var array $result
+     *
+     * @dataProvider providerExecBefor
+     *
+     * @throws
+     */
+    public function testExecBefor(string $name, callable $handler, int $number, array $result)
+    {
+        Debug::setHandler($name, $handler, true);
+        abs($number);
+        $this->assertEquals($result, DebugTest::$args);
+    }
+
+    public function providerExecBefor() {
+        $handler0 = function($name, $arguments) {
+            DebugTest::$args = func_get_args();
+        };
+        return [
+            ['abs', $handler0, -7, ['abs', [-7]]],
+            ['abs', $handler0, 7, ['abs', [7]]],
+            ['abs', $handler0, -1, ['abs', [-1]]],
+            ['abs', $handler0, 0, ['abs', [0]]],
+        ];
+    }
+
+    /**
+     * @var string $name
+     * @var callable $handler
+     * @var int $number
+     * @var array $result
+     *
+     * @dataProvider providerExecAfter
+     *
+     * @throws
+     */
+    public function testExecAfter(string $name, callable $handler, int $number, array $result)
+    {
+        Debug::setHandler($name, $handler, false);
+        abs($number);
+        $this->assertEquals($result, DebugTest::$args);
+    }
+
+    public function providerExecAfter() {
+        $handler0 = function($name, $arguments, $result) {
+            DebugTest::$args = func_get_args();
+        };
+        return [
+            ['abs', $handler0, -7, ['abs', [-7], 7]],
+            ['abs', $handler0, 7, ['abs', [7], 7]],
+            ['abs', $handler0, -1, ['abs', [-1], 1]],
+            ['abs', $handler0, 0, ['abs', [0], 0]],
         ];
     }
 }
